838516cc60bc07db173e378a5c3f35ad7ce9f49a
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-21 22:28:00 -0300

    Fazer com que a lista pegue os items

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { List, ListItem, ListItemText } from "@mui/material";
+
+export function TodoList ({ items }) {
+
+    function generate(element) {
+        return [items].map((value)=> (
+            React.cloneElement(element, {
+                key: value
+            }
+        )));
+    }
+
+    return (
+        <List>
+            {
+                generate (
+                    <ListItem>
+                        <ListItemText primary={'teste'} />
+                    </ListItem>
+                )
+            }
+        </List>
+    )
+}
