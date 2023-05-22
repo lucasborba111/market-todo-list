5475aad5728a403407fb790286f7090906643563
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-21 22:27:26 -0300

    Fazer com que a lista seja enviada ao todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { TodoList } from './components/TodoList'
 import { TodoListForm } from './components/TodoListForm'
 
 function App() {
   return (
     <>
+      <TodoList items={{producName: 'Maça', quantity: 3}}/>
       <TodoListForm/>
     </>
   )
