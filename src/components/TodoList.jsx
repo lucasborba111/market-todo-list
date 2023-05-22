b766dd6f8c3e8bd00ec6ebb4bb210c815a8c028e
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-21 22:31:27 -0300

    Fazer com que a lista receba os items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { List, ListItem, ListItemText } from "@mui/material";
 
 export function TodoList ({ items }) {
-
     function generate(element) {
         return [items].map((value)=> (
             React.cloneElement(element, {
