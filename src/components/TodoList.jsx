897b6d83d17aa840d556a28368797ec9d3a19bbf
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-22 23:24:15 -0300

    Lista de itens

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,24 +1,42 @@
 import * as React from 'react';
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Grid, List, ListItem, ListItemText, IconButton, Checkbox } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import styled from '@emotion/styled';
 
-export function TodoList ({ items }) {
-    function generate(element) {
-        return [items].map((value)=> (
-            React.cloneElement(element, {
-                key: value
-            }
-        )));
-    }
+export function TodoList ({ items, removeItem, checkHandler }) {
+    const Item = styled(ListItem)({
+        backgroundColor: '#fff',
+        "&:hover": {
+            backgroundColor: '#eeeeee',
+            borderBottom: 'solid 2px #dedede',
+            transition: 'background 0.3s'
+        }
+    });
 
     return (
-        <List>
-            {
-                generate (
-                    <ListItem>
-                        <ListItemText primary={'teste'} />
-                    </ListItem>
-                )
-            }
-        </List>
+        <Grid sx={{ flex: 0.6, display:'flex', flexDirection: 'column', height: '100%' }}>
+            <h3>Lista de produtos:</h3>
+            <List component="nav" aria-label="main mailbox folders">
+                { 
+                    items.map((record, index)=> {
+                        return (
+                            <Item key={index} secondaryAction={
+                                <Grid>
+                                    <Checkbox value={record.checked} checked={record.checked} onChange={() => checkHandler(index)}/>
+                                    <IconButton edge="end" aria-label="delete" onClick={() => removeItem(index)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </Grid>
+                              }>
+                                <ListItemText 
+                                    primary={record.productName} 
+                                    secondary={`Quantidade: ${record.quantity || 0}`} 
+                                />
+                            </Item>
+                        )
+                    })
+                }
+            </List>
+        </Grid>
     )
 }
