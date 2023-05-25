65e76836a7c249d1da00a5f6aad3c559a40828d8
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-24 21:33:29 -0300

    Criado o arquivo novo arquivo para os metodos

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,10 @@ import { TodoListForm } from './components/TodoListForm'
 function App() {
   const [ list, setList ] = useState([]);
 
-  function createItem(productName, quantity, checked = false) {
-    if(!(productName.current.value).trim()) return;
-    if(list.length === 5) return alert('Número máximo de itens atingido');
-
-    setList((prevList) => [
-      ...prevList,
-      {
-        productName: productName.current.value,
-        quantity: quantity.current.value,
-        checked: checked
-      }
-    ]);
-  }
-
-  function removeItem (index) {
-    setList(list.filter((_, position) => position !== index));
-  }
-
-  function checkHandler (index) {
-    const newList = list.map((record, position) => {
-      if(position === index) {
-        record.checked = !record.checked
-      }
-      return record;
-    });
-
-    setList(newList);
-  }
-
   return (
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: '100%'}}>
-      <TodoListForm createItem={createItem} />
-      <TodoList items={list} removeItem={removeItem} checkHandler={checkHandler}/>
+      <TodoListForm list={list} setList={setList} />
+      <TodoList list={list} setList={setList}/>
     </div>
   )
 }
