171b65b63ebdae39d97654ff0d5999a923000661
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-24 21:49:26 -0300

    Passando params para função de criar items

--- src/components/TodoListForm.jsx
+++ src/components/TodoListForm.jsx
@@ -1,15 +1,16 @@
 import { useRef } from "react";
 import { Grid, TextField , Button } from "@mui/material";
+import { createItem } from '../Handlers'
 
-export function TodoListForm({ createItem }) {
+export function TodoListForm({ list, setList }) {
     const productName = useRef('');
     const quantity = useRef('');
   
     return (
-      <Grid container direction='column' sx={{flex: 0.3}}>
-        <TextField name="productName" inputRef={productName} type="text" label="Produto" variant="standard" />
-        <TextField name="quantity" inputProps={{min: 0, step: 1}} inputRef={quantity} type="number" label="Quantidade" variant="standard" />
-        <Button onClick={() => createItem(productName, quantity)} > Adicionar </Button>
+      <Grid container direction='column' sx={{ flex: 0.3 }}>
+        <TextField name="productName" inputRef={ productName } type="text" label="Produto" variant="standard" />
+        <TextField name="quantity" inputProps={{ min: 0, step: 1 }} inputRef={ quantity } type="number" label="Quantidade" variant="standard" />
+        <Button onClick={() => createItem(list, setList, productName, quantity)} > Adicionar </Button>
       </Grid>
     );
 }
