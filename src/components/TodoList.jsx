10b4f2220b506fe2fa58696150a1c3cff50414b1
lucasborba111 <lucasborgesborba@gmail.com>
2023-05-24 21:48:12 -0300

    Passando ao item somente as props

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,38 +1,21 @@
 import * as React from 'react';
-import { Grid, List, ListItem, ListItemText, IconButton, Checkbox } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import styled from '@emotion/styled';
+import { Grid, List } from "@mui/material";
+import { Item } from './Item';
 
-export function TodoList ({ items, removeItem, checkHandler }) {
-    const Item = styled(ListItem)({
-        backgroundColor: '#fff',
-        "&:hover": {
-            backgroundColor: '#eeeeee',
-            borderBottom: 'solid 2px #dedede',
-            transition: 'background 0.3s'
-        }
-    });
-
+export function TodoList ({ list, setList }) {
     return (
         <Grid sx={{ flex: 0.6, display:'flex', flexDirection: 'column', height: '100%' }}>
             <h3>Lista de produtos:</h3>
             <List component="nav" aria-label="main mailbox folders">
                 { 
-                    items.map((record, index)=> {
+                    list.map((record, index) => {
                         return (
-                            <Item key={index} secondaryAction={
-                                <Grid>
-                                    <Checkbox value={record.checked} checked={record.checked} onChange={() => checkHandler(index)}/>
-                                    <IconButton edge="end" aria-label="delete" onClick={() => removeItem(index)}>
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </Grid>
-                              }>
-                                <ListItemText 
-                                    primary={record.productName} 
-                                    secondary={`Quantidade: ${record.quantity || 0}`} 
-                                />
-                            </Item>
+                            <Item 
+                                record = { record } 
+                                index = { index } 
+                                setList = { setList }
+                                list = { list }
+                            />
                         )
                     })
                 }
